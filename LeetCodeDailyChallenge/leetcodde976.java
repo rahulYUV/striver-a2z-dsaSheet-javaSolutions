161728c34347831f4515e118c933c1eb42c64483
rahulYUV <rs6205903996@gmail.com>
2025-09-28 12:18:21 +0530

    28th sept leetcode Daily

---
+++ LeetCodeDailyChallenge/leetcodde976.java
@@ -0,0 +1,17 @@
+package LeetCodeDailyChallenge;
+import java.util.Arrays;
+
+public class leetcodde976 {
+    class Solution {
+        public int largestPerimeter(int[] nums) {
+          Arrays.sort(nums);
+      
+          for (int i = nums.length - 1; i > 1; --i)
+            if (nums[i - 2] + nums[i - 1] > nums[i])
+              return nums[i - 2] + nums[i - 1] + nums[i];
+      
+          return 0;
+        }
+      }
+      
+}
